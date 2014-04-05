9101e5ab88c84763d94ef74e5f08c3eef9c5e053
Jeff Buchbinder <jeffreymbuchbinder@gmail.com>
2014-04-05 12:29:39 -0400

    Update script.

--- prores-proxies/prores-proxies.sh
+++ prores-proxies/prores-proxies.sh
@@ -3,20 +3,36 @@
 #	prores-proxies.sh
 #	@jbuchbinder
 #
-#	Convert all files in the current directory to h.264 proxies in
-#	a subdirectory called "proxy".
+#	Convert all files in the current directory (or a specified set of
+#	directories) to h.264 proxies in subdirectories of the directory(s)
+#	containing the source files, called "proxy".
 #
 
 ffmpeg="/usr/bin/ffmpeg"
 subdir="proxy"
 
-for i in *.mov *.MOV; do
-	o="$(dirname "${i}")/${subdir}/$(basename "${i}")"
-	mkdir -p "$(dirname "$o")"
-	echo "$(date)| Converting $i -> $o"
-	time ${ffmpeg} \
-		-y \
-		-i "$i" \
-		-vcodec libx264 -acodec aac -strict -2 -preset ultrafast \
-		"${o}"
+files=""
+if [ $# -gt 0 ]; then
+	# Use args
+	for d in $*; do
+		files="$files $d/*.mov"
+	done
+else
+	files="*.mov"
+fi
+
+for i in $files; do
+	if [ -f "$i" ]; then
+		o="$(dirname "${i}")/${subdir}/$(basename "${i}")"
+		mkdir -p "$(dirname "$o")"
+		echo "$(date)| Converting $i -> $o"
+		time ${ffmpeg} \
+			-y \
+			-i "$i" \
+			-vcodec libx264 -acodec aac -strict -2 -preset ultrafast \
+			"${o}"
+		echo "$(date)| Completed proxy : $o"
+	else
+		echo "$(date)| Skipping non-existing file ${i}"
+	fi
 done
